6edec060b3baecf5d65f20bbc4116584ec5c907e
default <william.provost@stu.ocsb.ca>
2025-11-25 01:50:05 +0000

    Did backwards program by 5 in Ts and Go

--- Unit4A.go
+++ Unit4A.go
@@ -1,6 +1,6 @@
 // Author: William Provost
 // Version: 1.0.0
-// Date: 2025-11-24
+// Date: 2025-11-25
 // Fileoverview: This program counts backward by 5's from 100 down to 0.
 
 package main
@@ -8,17 +8,14 @@ package main
 import "fmt"
 
 func main() {
-    // variable declaration
-    currentValue := 100
+	currentValue := 100
 
-    // output header
-    fmt.Println("Backward count by 5's from 100 to 0:")
+	fmt.Println("Backward count by 5's from 100 to 0:")
 
-    // loop to count backward by 5's
-    for currentValue >= 0 {
-        fmt.Println(currentValue)
-        currentValue -= 5
-    }
+	for currentValue >= 0 {
+		fmt.Println(currentValue)
+		currentValue -= 5
+	}
 
-    fmt.Println("\nDone.")
+	fmt.Println("\nDone.")
 }
